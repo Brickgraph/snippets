7f1c3aac9ff72a7062f47b2de5682032669805c5
David Bender <bender2242@gmail.com>
2024-08-21 12:47:45 +0100

    first commit

---
+++ power-query/brickgraph-reports-extract-entities.m
@@ -0,0 +1,30 @@
+let
+    ApiUrl = "https://public.brickgraph.io/reports/ORG-UID/entity/ENTITY-TYPE",
+    ApiKey = "API-KEY",
+    Limit = 100,
+
+    FetchData = (Offset) =>
+        let
+            Source = Json.Document(Web.Contents(ApiUrl, [
+                Query = [
+                    api_key = ApiKey,
+                    offset = Text.From(Offset),
+                    limit = Text.From(Limit)
+                ]
+            ])),
+            data = Source[data]
+        in
+            data,
+
+    FetchAllPages = (offset) =>
+        let
+            CurrentPage = FetchData(offset),
+            NextPage = if List.Count(CurrentPage) < Limit then null else @FetchAllPages(offset + Limit),
+            CombinedData = List.Combine({CurrentPage, NextPage})
+        in
+            if NextPage = null then CurrentPage else CombinedData,
+
+    AllData = FetchAllPages(0),
+    ToTable = Table.FromList(AllData, Splitter.SplitByNothing(), null, null, ExtraValues.Error)
+in
+    ToTable
---
+++ power-query/brickgraph-reports-pagination.m
@@ -0,0 +1,45 @@
+let
+    ApiUrl = "https://public.brickgraph.io/reports/ORG-UID/REPORT-CODE",
+    ApiKey = "API-KEY",
+    Limit = 100,
+
+    FetchData = (Offset) =>
+    let
+        Source = Json.Document(Web.Contents(ApiUrl, [
+            Query = [
+                api_key = ApiKey,
+                offset = Text.From(Offset),
+                limit = Text.From(Limit)
+            ]
+        ])),
+        CheckRecords = if Source[records] = 0 then null else Source,
+        ProcessData = if CheckRecords = null then null else
+            let
+                data = Source[data],
+                tableFromData = Table.FromList(data, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+                expandedColumn = Table.ExpandRecordColumn(tableFromData, "Column1", {"record"}, {"record"}),
+                // Get all column names
+                firstRecord = expandedColumn{0}[record],
+                allFieldNames = if firstRecord <> null then Record.FieldNames(firstRecord) else {},
+                // Dynamically expand all fields
+                finalTable = Table.ExpandRecordColumn(expandedColumn, "record", allFieldNames, allFieldNames)
+            in
+                finalTable
+    in
+        ProcessData,
+
+    FetchAllPages = (offset) =>
+        let
+            CurrentPage = FetchData(offset),
+            CombinedData = if CurrentPage = null then
+                        Table.FromRecords({})  // Return an empty table when there's no more data
+                    else if offset = 0 then
+                        Table.Combine({CurrentPage, @FetchAllPages(offset + Limit)})
+                    else
+                        Table.Combine({CurrentPage, @FetchAllPages(offset + Limit)})
+        in
+            CombinedData,
+
+    AllData = FetchAllPages(0)
+in
+    AllData
